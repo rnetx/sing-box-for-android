d05258929fd67057ee79f285f75cbae19021abcd
yaotthaha <yaotthaha@foxmail.com>
2023-06-29 13:07:51 +0800

    update

--- README.md
+++ README.md
@@ -30,3 +30,19 @@ with this application without prior consent.
 
 Under the license, that forks of the app are not allowed to be listed on F-Droid or other app stores
 under the original name.
+
+## 用法
+
+1. Fork 本项目
+2. 本地生成 Android JKS 签名文件，并上传替换 app/android.jks (自己 Google)
+3. 添加 Secrets
+```
+仓库 ==> settings ==> Secrets and variables ==> Actions ==> New repository secret
+
+添加如下变量，设置 JKS 密码、别名、别名密码：(自己 Google)
+KEYSTORE_PASS
+ALIAS_NAME
+ALIAS_PASS
+```
+4. 修改 build_libbox.sh，你可以改成自己想要的仓库和分支，这步不懂可以什么都不动
+5. 在 Action 中触发构建
--- build_libbox.sh
+++ build_libbox.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 ##### 除非你知道自己在干什么，否则什么都不要动 #####
+# 想使用指定分支请修改链接，注意不要去掉后面的`sing-box`
 git clone https://github.com/yaotthaha/sing-box-pub sing-box
+###### !! 不要动 !! ######
 cd sing-box
+##########################
+# 这里可以切换分支
 git checkout dev-next-yaott
+##########################
+# https://github.com/yaotthaha/sing-box-pub@dev-next-yaott 编译 with_clash_ui 需要，其他分支可以注释掉 #########
 git submodule init
 git submodule update
 ####################
+# 这里可以修改 tags
 tags="with_dhcp,with_lwip,with_acme,with_gvisor,with_v2ray_api,with_quic,with_wireguard,with_utls,with_reality_server,with_clash_api,with_shadowsocksr,with_grpc,with_ech,with_proxyprovider,with_clash_ui,with_multiaddr"
+####################
 ###### !! 不要动 !! ######
 make lib_install
 version=$(git rev-parse --short HEAD)
