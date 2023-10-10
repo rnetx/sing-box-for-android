57ee771eaafdb77ca33d5e3742dc4f96b3adb6d7
yaotthaha <yaotthaha@foxmail.com>
2023-10-10 18:11:01 +0800

    Remove shadowsocksr build tag

--- build_libbox.sh
+++ build_libbox.sh
@@ -11,9 +11,10 @@ git checkout dev-yaott
 # https://github.com/yaotthaha/sing-box-pub@dev-next-yaott 编译 with_clash_ui 需要，其他分支可以注释掉 #########
 # git submodule init
 # git submodule update
+make init_yacd
 ####################
 # 这里可以修改 tags
-tags="with_dhcp,with_lwip,with_gvisor,with_v2ray_api,with_quic,with_wireguard,with_utls,with_reality_server,with_clash_api,with_shadowsocksr,with_grpc,with_ech,with_proxyprovider,with_ruleprovider,with_clash_ui"
+tags="with_dhcp,with_lwip,with_gvisor,with_v2ray_api,with_quic,with_wireguard,with_utls,with_reality_server,with_clash_api,with_grpc,with_ech,with_proxyprovider,with_ruleprovider,with_clash_dashboard"
 ####################
 ###### !! 不要动 !! ######
 make lib_install
